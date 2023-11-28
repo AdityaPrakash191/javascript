109c1a6c2841e729575b5233b1fd601bd5261dfe
AdityaPrakash191 <133683995+AdityaPrakash191@users.noreply.github.com>
2023-11-28 07:44:24 +0000

    nothing

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,9 +8,9 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null > 0); false
+// console.log(null == 0);false
+// console.log(null >= 0);true
 // the reason is that an equity check == and comparison < <= >= > works differently.
 // comparison converts null to a number, treating it as 0
 // that's why null >= 0 is true and null > 0 is false
